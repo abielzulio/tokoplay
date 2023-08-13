1c343bfc6a1bbc14844046d5bfc00b17067ee3f4
Abiel Zulio M <abielzm@gmail.com>
2023-08-13 21:49:31 +0700

    server(wip): add `video` route to index

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,28 @@
 import express, { Express, Request, Response } from "express"
 import dotenv from "dotenv"
+import mongoConnect from "./lib/mongo"
+import videoRouter from "./routers/video.router"
 
 dotenv.config()
 
 const app: Express = express()
 const port = process.env.PORT
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Tokoplay API")
+const server = app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
 })
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+app.use(express.json())
+
+app.use("/v1", videoRouter)
+
+mongoConnect(process.env.DB_URL, "tokoplay", () => {
+  server
+})
+
+process.on("SIGTERM", () => {
+  server.close(() => {
+    console.log("Process terminated")
+    process.kill(process.pid, "SIGTERM")
+  })
 })
