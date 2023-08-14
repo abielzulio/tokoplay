2433c59fdb597a5f1d4cb0aafd53cb7285989646
Abiel Zulio M <abielzm@gmail.com>
2023-08-14 19:39:32 +0700

    client(`app`): router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,16 @@
-import { Route } from "wouter"
+import { Route, Switch } from "wouter"
 import "./App.css"
 import HomePage from "./pages/home.page"
+import VideoPage from "./pages/video.page"
 
 function App() {
-  return <Route path="/" component={HomePage} />
+  return (
+    <Switch>
+      <Route path="/video/:id" component={VideoPage} />
+      <Route path="/" component={HomePage} />
+      <Route component={HomePage} />
+    </Switch>
+  )
 }
 
 export default App
