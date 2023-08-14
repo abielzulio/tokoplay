eb9e4de195d80b1f9af96292ec39fda1ae2cd5dc
Abiel Zulio M <abielzm@gmail.com>
2023-08-14 20:39:41 +0700

    server(`randomizeCategory`): docs

--- server/utils/index.ts
+++ server/utils/index.ts
@@ -1,3 +1,7 @@
+/**
+ * Function to randomize category from "Live", "Eksplor", and "Promo"
+ * @returns a random category
+ */
 export const randomizeCategory = (): string => {
   const category = ["Live", "Eksplor", "Promo"]
   return category[Math.floor(Math.random() * category.length)]
