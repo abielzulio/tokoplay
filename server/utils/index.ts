df1c804815b4250f43a58e30c9112aaad708f072
Abiel Zulio M <abielzm@gmail.com>
2023-08-15 10:21:58 +0700

    refactor(`server`): func

--- server/utils/index.ts
+++ server/utils/index.ts
@@ -2,7 +2,7 @@
  * Function to randomize category from "Live", "Eksplor", and "Promo"
  * @returns a random category
  */
-export const randomizeCategory = (): string => {
+export function randomizeCategory(): string {
   const category = ["Live", "Eksplor", "Promo"]
   return category[Math.floor(Math.random() * category.length)]
 }
